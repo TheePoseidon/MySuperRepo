719ee3d2a21a9772fb517ac533ae60034a0c6969
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-09-15 21:17:53 +0300

    What else

---
+++ Genesis_Jay/Statements.py
@@ -0,0 +1,9 @@
+temperature = 10
+
+if temperature > 40:
+    print("It is very hot")
+    print("Take a lot of water")
+elif temperature > 20:
+    print("Enjoy your day")
+else :
+    print("Very cold day")
