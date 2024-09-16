f14fb0771c74013c943cdb249806b213a578abbd
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-09-16 18:45:53 +0300

    Types of lists

---
+++ Genesis_Jay/lists.py
@@ -0,0 +1,16 @@
+names = ("Jay", "James", "Kay", "Mawia", "June")
+print(names)
+print(names[0:3])
+
+print("Jay" in names)
+for item in names:
+    print(item)
+
+numbers = range(5)
+print(numbers)
+for number in numbers:
+    print (number)
+
+
+for years in range(7, 50, 2):
+    print(years)
