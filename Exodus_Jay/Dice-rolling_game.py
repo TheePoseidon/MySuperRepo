1e6a3c31b38d198b00313c46dfbf693610929f8d
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-09-25 10:40:50 +0300

    Roll or die

--- Exodus_Jay/Dice-rolling_game.py
+++ Exodus_Jay/Dice-rolling_game.py
@@ -1,7 +1,14 @@
-import numbers
+import random
 
-Trial = input("Roll the dice?(y/n): ").lower()
-if Trial == "y":
-    print(input("Lets go: "))
-if Trial == "n":
-    pr
+while True:
+    Trial = input('Roll the dice? (y,n): ').lower()
+    if Trial == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'({die1}, {die2})')
+    elif Trial == 'n':
+        print('Thank you for playing!')
+        break
+    else:
+        print('Invalid choice!')
+
