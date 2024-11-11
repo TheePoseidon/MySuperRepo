f0f86330593dccb2c2434206236c08b31ffed3d6
TheePoseidon <169036596+TheePoseidon@users.noreply.github.com>
2024-11-11 20:36:21 +0300

    Prints greater than 1000

---
+++ Lets-talk_C/great.C
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    //Enter a number
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    //If number is greater than 1000
+    if (number > 1000) {
+        printf("The number is greater than 1000./n");
+    } else {
+        printf("The number is greater than 1000./n");
+    }
+
+    return 0;
+}
